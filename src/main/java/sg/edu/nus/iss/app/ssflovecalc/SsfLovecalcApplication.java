81ed136e875247ceb8ae83aa66acb11c8ebe4bad
Wee Seng <wee.bscl@gmail.com>
2023-01-19 09:46:39 +0800

    init commit

---
+++ src/main/java/sg/edu/nus/iss/app/ssflovecalc/SsfLovecalcApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.app.ssflovecalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsfLovecalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsfLovecalcApplication.class, args);
+	}
+
+}
